d5138b5bbf956fd15b3ec13f314cc99373c78b00
AndreaBeyer <48089121+AndreaBeyer@users.noreply.github.com>
2019-03-05 11:34:41 +0100

    adds

---
+++ RayonGraphique/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public double rayon, circonference, surface;
+        bool ok;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            string rayonString = textBox1.Text;
+            try
+            {
+                rayon = double.Parse(rayonString);
+                ok = true;
+            }
+            catch
+            {
+                ok = false;
+            }
+        }
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!ok)
+            {
+                MessageBox.Show("l'entree est invalide");
+            }
+            else
+            {
+                circonference = Math.PI * rayon * 2;
+
+                surface = Math.PI * Math.Pow(rayon, 2);
+                textBox2.Text = Math.Round(circonference,2).ToString();
+                textBox3.Text = Math.Round(surface,2).ToString();
+
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
